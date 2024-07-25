1c1986a8920a0493cc846a36697b9760a9d74d65
workspaceuser <workspaceuser@plot.ly>
2024-07-25 15:29:52 +0000

    remote debug prints

--- tasks.py
+++ tasks.py
@@ -106,7 +106,6 @@ def append_new_observations():
         ago1 = 45-((days+1)*5)
         ago2 = 45-(days*5)
         logger.info('checking for duplicates in days: ' + str(ago1) + ' to ' + str(ago2) + ' ago.' )
-        print('checking for duplicates in days: ' + str(ago1) + ' to ' + str(ago2) + ' ago.' )
         stored_df = db.get_between_days_ago(ago1, ago2)
         stored_df = stored_df.assign(source='db')
         logger.info(str(stored_df.shape[0]) + ' obervations found in database.')
@@ -116,7 +115,6 @@ def append_new_observations():
         df.drop(df[['latitude', 'longitude', 'observation_depth','millis']].round(PRECISION).duplicated().loc[lambda latitude: latitude].index, inplace=True)
         df = df[df['source']=='erddap']
         logger.info(str(df.shape[0]) + ' observations that are not stored remain after checking this day range.')
-        print(str(df.shape[0]) + ' observations that are not stored remain after checking this day range.')
 
     df = df[columns]
     df = df.dropna(subset=['latitude','longitude'], how='any')
@@ -134,7 +132,6 @@ def append_new_observations():
     # and the SQLAlchemy engine we created above. When if_exists='append' we add the rows to our table
     # and when if_exists='replace', a new table overwrites the old one.
     logger.info('Updating data...')
-    print('updateing data')
     if df.shape[0] > 0:
         df.to_csv('../mount/update.csv', index=False)
         df.to_sql(constants.data_table, constants.postgres_engine, if_exists='append', index=False, chunksize=500, method='multi')
@@ -148,7 +145,6 @@ def append_new_observations():
     counts_df.reset_index(inplace=True)
 
     logger.info('Updating counts...')
-    print('updating counts')
     counts_df.to_csv('../mount/counts.csv', index=False)
     counts_df.to_sql(constants.counts_table, constants.postgres_engine, if_exists='replace', index=False, method='multi')
     logger.info('Updating locations...')
